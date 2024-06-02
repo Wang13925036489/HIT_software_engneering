c513a3830fc2de72b88b37f3b0adefae79b4c906
Wang13925036489 <shouxiao57497@163.com>
2024-06-02 22:55:21 +0800

    代码优化

--- lab1gui_v2.py
+++ lab1gui_v2.py
@@ -10,7 +10,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QTextEdit, Q
 def read_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         return file.read()
-
+ 
 def create_directed_graph(text): 
     words = re.findall(r'\b\w+\b', text.lower())
     graph = nx.DiGraph()
